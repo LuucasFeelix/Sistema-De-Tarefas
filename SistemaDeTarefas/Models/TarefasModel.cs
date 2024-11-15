6919b0250bafd2be30f83e8ff134bd3af4b397d5
Lucas Felix Da Silva <luucasfeelix1925@hotmail.com>
2024-11-15 20:21:51 -0300

    Realizado criação de controllers, data, models e repositorio

---
+++ SistemaDeTarefas/Models/TarefasModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class TarefasModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? descricao { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ SistemaDeTarefas/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios.Interfaces;
+
+namespace SistemaDeTarefas.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaTarefasDBContex _dbContext;
+        public UsuarioRepositorio(SistemaTarefasDBContex sistemaTarefasDBContex)
+        {
+            _dbContext = sistemaTarefasDBContex;
+        }
+
+        public async Task<UsuarioModel> BuscarPorId(int id)
+        {
+            return await _dbContext.Usuarios.FirstOrDefaultAsync(X => X.Id == id);
+        }
+
+        public Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
